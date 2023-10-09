8be5a9797a9621aae58510c3c3b5d6227c3fd2a4
Valmir Cardoso Ribeiro <valmir@hospitalsaojose.com.br>
2023-10-09 10:22:33 -0300

    Exercício 02

--- Exercicios_Interface_GUI/02.py
+++ Exercicios_Interface_GUI/02.py
@@ -1,15 +1,16 @@
-# Exercício 01 - Conversão de estatura
+# Exercício 02 - Velocidade Média
 
 from tkinter import *
 
 # Função
 def calcular():
-    altura_cm = float(txt_altura.get())
-    resultado.set(altura_cm*100)
+    deslocamento = float(txt_deslocamento.get())
+    tempo = float(txt_tempo.get())
+    resultado.set(deslocamento/tempo)
 
 # Configuração da janela
 janela = Tk()
-janela.title("Conversão de estatura")
+janela.title("Velocidade média")
 
 largura_janela = 400
 altura_janela = 300
@@ -22,7 +23,7 @@ y = (altura_tela - altura_janela) // 2
 
 janela.geometry(f"{largura_janela}x{altura_janela}+{x}+{y}")
 
-fonte = ("Arial", 12)
+fonte = ("Arial", 11)
 
 # Frame para centralizar os itens na tela
 tela = Frame(janela)
@@ -30,13 +31,18 @@ tela.pack(expand=True, padx=20, pady=20)
 
 
 # Widgets (Exibição - Labels)
-titulo = Label(tela, text="Conversão de estatura", font=("Arial", 18))
+titulo = Label(tela, text="Velocidade média", font=("Arial", 18))
 titulo.pack(pady=10)
 
-lbl_altura = Label(tela, text='Informe sua estatura (em metros):',font=fonte)
-lbl_altura.pack()
-txt_altura = Entry(tela)
-txt_altura.pack(pady=5)
+lbl_deslocamento = Label(tela, text='Informe a variação de deslocamento (em metros):',font=fonte)
+lbl_deslocamento.pack()
+txt_deslocamento = Entry(tela)
+txt_deslocamento.pack(pady=5)
+
+lbl_tempo = Label(tela, text='Informe a variação do tempo percorrido (em segundos):',font=fonte)
+lbl_tempo.pack()
+txt_tempo = Entry(tela)
+txt_tempo.pack(pady=5)
 
 btn_calcular = Button(tela, text='Calcular',font=fonte,command=calcular)
 btn_calcular.pack(pady=5)
@@ -47,6 +53,6 @@ lbl_resultado = Label(tela, textvariable=resultado,font=fonte)
 lbl_resultado.pack(pady=5)
 
 lbl_dev = Label(tela,text="Desenvolvido por Valmir - 2023")
-lbl_dev.pack(pady=33)
+lbl_dev.pack(pady=8)
 
 tela.mainloop()
